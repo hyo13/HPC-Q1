9370e58a87f2b80f270ec8f25cc1efecec764d49
hyo13 <ho.or13@imperial.ac.uk>
2016-03-22 22:18:15 +0000

    HPC Q1 file start

---
+++ HPC-Q1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HPC-Q1
+//
+//  Created by hyo13 on 22/3/16.
+//  Copyright (c) 2016 hyo13. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int main() {
+    cout << "Sup World." << endl;
+    return 0;
+}
